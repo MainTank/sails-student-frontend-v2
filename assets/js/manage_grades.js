f8b54351fa296124c3ae4073636cff96e2b76566
Landen Wasserstrom <Landen.Wasserstrom@gmail.com>
2017-09-13 14:07:53 -0700

    added grades and assignments view

--- assets/js/manage_grades.js
+++ assets/js/manage_grades.js
@@ -1,31 +1,31 @@
 (function(){
 
   //function to delete record by settin id on form and then submitting the form
-  //sets value of student id in hidden delete form and submits form
+  //sets value of Grade id in hidden delete form and submits form
   //not completely ideal but wanted to take advantage of flash messages in sails
   function deleteRecord(record_id){
-    $("#deleteform input[name=student_id]").val(record_id);
+    $("#deleteform input[name=grade_id]").val(record_id);
     $("#deleteform").submit();
   }
 
-  function getStudent(record_id){
-    return $.get("http://localhost:1337/student/" + record_id, function(data){
-      console.log("got student");
+  function getGrade(record_id){
+    return $.get("http://localhost:1337/grade/" + record_id, function(data){
+      console.log("got grade");
     })
   }
 
   $(function(){
 
     //initialize variables for items in the DOM we will work with
-    let manageStudentForm = $("#manageStudentForm");
-    let addStudentButton = $("#addStudentButton");
+    let manageGradeForm = $("#manageGradeForm");
+    let addGradeButton = $("#addGradeButton");
 
-    //add student button functionality
-    addStudentButton.click(function(){
-      manageStudentForm.find("input[name=student_id]").val("");
-      document.getElementById("manageStudentForm").reset();
-      manageStudentForm.attr("action", "/create_student");
-      manageStudentForm.dialog({
+    //add Grade button functionality
+    addGradeButton.click(function(){
+      manageGradeForm.find("input[name=grade_id]").val("");
+      document.getElementById("manageGradeForm").reset();
+      manageGradeForm.attr("action", "/create_grade");
+      manageGradeForm.dialog({
         title: "Add Record",
         width: 700,
         modal: true,
@@ -35,20 +35,20 @@
           },
           "Submit": function() {
             //function to delete record
-            manageStudentForm.submit()
+            manageGradeForm.submit()
           }
         }
       });
     })
 
-  	$("#studentTable").on("click", "#editButton", function(e){
-      let recordId = $(this).data("studentid")
-      manageStudentForm.find("input[name=student_id]").val(recordId);
-      manageStudentForm.attr("action", "/update_student");
-      let student = getStudent(recordId);
+  	$("#gradeTable").on("click", "#editButton", function(e){
+      let recordId = $(this).data("gradeid")
+      manageGradeForm.find("input[name=grade_id]").val(recordId);
+      manageGradeForm.attr("action", "/update_grade");
+      let grade = getGrade(recordId);
 
-      //populate form when api call is done (after we get student to edit)
-      student.done(function(data){
+      //populate form when api call is done (after we get grade to edit)
+      grade.done(function(data){
         $.each(data, function(name, val){
             var $el = $('[name="'+name+'"]'),
                 type = $el.attr('type');
@@ -66,7 +66,7 @@
         });
       })
 
-      manageStudentForm.dialog({
+      manageGradeForm.dialog({
         title: "Add Record",
         width: 700,
         modal: true,
@@ -76,15 +76,15 @@
           },
           Submit: function() {
 
-            manageStudentForm.submit()
+            manageGradeForm.submit()
           }
         }
       });
     })
 
 
-    $("#studentTable").on("click", "#deleteButton", function(e){
-      let recordId = $(this).data("studentid")
+    $("#gradeTable").on("click", "#deleteButton", function(e){
+      let recordId = $(this).data("gradeid")
       $("#deleteConfirm").dialog({
         title: "Confirm Delete",
         modal: true,
@@ -92,7 +92,7 @@
           Cancel: function() {
             $( this ).dialog( "close" );
           },
-          "Delete Student": function() {
+          "Delete Grade": function() {
             //function to delete record
             deleteRecord(recordId);
           }
@@ -102,7 +102,7 @@
 
   })
 
-  $('#studentTable').DataTable( {
+  $('#gradeTable').DataTable( {
     dom: 'Bfrtip',
     buttons: [
         'copy', 'csv', 'excel', 'pdf', 'print'
